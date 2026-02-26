401486a04ecd53e104029a526caa9a19d586faf3
Gilberto-IsPa <gilberto.islas.palomino@gmail.com>
2026-02-25 18:51:26 -0700

    Leetcode:

---
+++ Leetcode/LongestSubstringWithoutRepeatingChar.py
@@ -0,0 +1,21 @@
+"""
+Given a string s, find the length of the longest substring without duplicate characters.
+
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hash = {}
+        left = 0
+        right = 0
+        maxlen = 0
+
+        for right in range(len(s)):
+            while s[right] in hash:
+                hash.pop(s[left])
+                left += 1
+            
+            hash[s[right]] = right
+            maxlen = max(maxlen, right - left + 1)
+
+        return maxlen
